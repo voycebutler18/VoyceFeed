ba2583015f329d940597ad5cd3668775478e3d71
voycebutler18 <voycebutler1@gmail.com>
2025-07-18 23:26:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ class VideoLike(db.Model):
     __tablename__ = 'video_like'
 
     def check_password(self, password):
-        return bcrypt.check_password_hash(self.password_hash, password)
+    return bcrypt.check_password_hash(self.password_hash, password)
     
     def get_display_name(self):
         return self.email.split('@')[0]
